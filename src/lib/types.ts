fa944c5bc397931e2d0dd83ac7a99401a5ac9254
Andre Lemay <lemay.andre@gmail.com>
2020-01-09 09:24:04 -0500

    Fixed indentation

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,23 +1,23 @@
 export interface Career {
-  name: string
-  description: string
-  careerSkills: string[]
-  specializations: Specialization[]
+	name: string
+	description: string
+	careerSkills: string[]
+	specializations: Specialization[]
 }
 
 export interface Specialization {
-  name: string
-  subtitle: string
+	name: string
+	subtitle: string
 	description: string
-  careerSkills: string[]
-  talents: Talent[]
+	careerSkills: string[]
+	talents: Talent[]
 }
 
 export interface Talent {
-  name: string
-  description: string
-  row: number
-  column: number
-  passive: boolean
-  connections: boolean[]
+	name: string
+	description: string
+	row: number
+	column: number
+	passive: boolean
+	connections: boolean[]
 }

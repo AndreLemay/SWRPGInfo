2358608d8a4395d324e02e5feb47bfea671ef1ab
Andre Lemay <lemay.andre@gmail.com>
2020-01-10 21:51:56 -0500

    Added image to home page Further style tweaks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,11 @@
 import * as React from 'react'
-// import * as Data from '../../content/data.json'
-// import { Container, Row, Col } from 'react-bootstrap'
-// import CareerItem from '../components/CareerItem'
 import Layout from '../components/Layout'
+import logo from '../../content/img/SWEotE.png'
 
 export default _ => {
 	return (
 		<Layout>
-			{/* <Container fluid style={{ marginTop: '10px' }} className="mainBody">
-				<Row>
-					{Data.careers.map((c, ind) => {
-						return (
-							<Col xs={12} md={6} lg={4} style={{ marginBottom: '10px' }} key={ind}>
-								<CareerItem {...c} />
-							</Col>
-						)
-					})}
-				</Row>
-			</Container> */}
-			HOME TEMP
+			<img className="logo" src={logo} />
 		</Layout>
 	)
 }

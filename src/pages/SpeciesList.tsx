396b10124faa46f1a5e2ecb40292ed96c343840a
Andre Lemay <lemay.andre@gmail.com>
2020-01-19 19:33:02 -0500

    Converted species list to use DataTable

--- src/pages/SpeciesList.tsx
+++ src/pages/SpeciesList.tsx
@@ -2,22 +2,78 @@ import * as React from 'react'
 import * as Data from '../../content/data.json'
 import {
 	Container,
-	Row,
-	Col,
-	Table,
 	OverlayTrigger,
 	Tooltip,
 	Image
 } from 'react-bootstrap'
 import Layout from '../components/Layout'
+import DataTable from '../components/DataTable'
 import { Link } from 'gatsby'
-import { Species } from '../lib/types'
 
 export default () => {
 	return (
 		<Layout>
 			<Container fluid>
-				<Row>
+				<DataTable
+					title="Species"
+					bordered
+					cols={[
+						{
+							label: 'Name',
+							field: 'name',
+							sortable: true,
+							render: data => {
+								let imgSrc = require(`../../content/img/${data.name.toLowerCase()}.jpg`)
+
+								return (
+									<OverlayTrigger
+										placement="right"
+										overlay={
+											// tslint:disable-next-line: jsx-wrap-multiline
+											<Tooltip id={`tooltip-${data.name}`}>
+												<Image className="popupImg" src={imgSrc} fluid />
+											</Tooltip>
+										}>
+										<Link to={`/Species/${data.name}`}>{data.name}</Link>
+									</OverlayTrigger>
+								)
+							}
+						},
+						{
+							label: 'Brawn',
+							field: 'brawn',
+							sortable: true
+						},
+						{
+							label: 'Agility',
+							field: 'agility',
+							sortable: true
+						},
+						{
+							label: 'Intellect',
+							field: 'intellect',
+							sortable: true
+						},
+						{
+							label: 'Cunning',
+							field: 'cunning',
+							sortable: true
+						},
+						{
+							label: 'Willpower',
+							field: 'willpower',
+							sortable: true
+						},
+						{
+							label: 'Presence',
+							field: 'presence',
+							sortable: true
+						}
+					]}
+					data={Data.species}
+					sort={{ field: 'name', dir: 'ASC' }}
+				/>
+				{/* <Row>
 					<Col>
 						<span>Species</span>
 					</Col>
@@ -76,7 +132,7 @@ export default () => {
 							</tbody>
 						</Table>
 					</Col>
-				</Row>
+				</Row> */}
 			</Container>
 		</Layout>
 	)
